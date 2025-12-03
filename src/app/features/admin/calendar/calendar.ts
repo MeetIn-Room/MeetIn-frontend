ad4352ab478de3479619f03d5ee6ce31d62b8365
cheugs <88487663+cheugs@users.noreply.github.com>
2025-12-03 13:12:14 +0100

    updates of the pages present

--- src/app/features/admin/calendar/calendar.ts
+++ src/app/features/admin/calendar/calendar.ts
@@ -1,11 +1,14 @@
- import { Component, OnInit, HostListener } from '@angular/core';
+ import { Component, OnInit} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { SidebarComponent } from '../../../shared/components/sidebar/sidebar';
+import { HeaderComponent } from '../../../shared/components/navbar/navbar';
+
 
 @Component({
   selector: 'app-calendar',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, SidebarComponent, HeaderComponent],
   templateUrl: './calendar.html',
   styleUrls: ['./calendar.css']
 })
--- src/app/features/admin/settings/settings.ts
+++ src/app/features/admin/settings/settings.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit, HostListener } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { SidebarComponent } from '../../../shared/components/sidebar/sidebar';
+import { HeaderComponent } from '../../../shared/components/navbar/navbar';
 
 @Component({
   selector: 'app-settings',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, SidebarComponent, HeaderComponent],
   templateUrl: './settings.html',
   styleUrls: ['./settings.css']
 })
